029c2060809258c6f688441da9c3b4fe27e2cd00
3dagger <dlqtn36@gmail.com>
2022-01-13 01:21:52 +0900

    fix: PresentationModule.kt Add

--- DaggerHiltNetworkConnection/build.gradle.kts
+++ DaggerHiltNetworkConnection/build.gradle.kts
@@ -2,7 +2,6 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-//        maven { url "https://oss.sonatype.org/content/repositories/snapshots/" }
         gradlePluginPortal()
     }
     dependencies {
@@ -10,11 +9,6 @@ buildscript {
         classpath(kr.dagger.buildsrc.BuildPlugins.hilt)
         classpath(kr.dagger.buildsrc.BuildPlugins.android)
         classpath(kr.dagger.buildsrc.BuildPlugins.ksp)
-//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-//        classpath "com.android.tools.build:gradle:$versions.gradleBuildTool"
-//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
-//        classpath "com.google.dagger:hilt-android-gradle-plugin:$versions.hiltCoreVersion"
-
     }
 }
 
@@ -24,10 +18,6 @@ allprojects {
         mavenCentral()
     }
 }
-
-//task clean(type: Delete) {
-//    delete rootProject.buildDir
-//}
 
 tasks.register("clean", Delete::class){
     delete(rootProject.buildDir)
