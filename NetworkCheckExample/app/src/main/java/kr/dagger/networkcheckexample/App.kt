1d77036548fc70d59f59af8468dfe2bb757385dd
3dagger <dlqtn36@gmail.com>
2023-11-21 16:23:32 +0900

    Network check example 추가

---
+++ NetworkCheckExample/app/src/main/java/kr/dagger/networkcheckexample/App.kt
@@ -0,0 +1,9 @@
+package kr.dagger.networkcheckexample
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
---
+++ NetworkCheckExample/app/src/main/java/kr/dagger/networkcheckexample/NetworkModule.kt
@@ -0,0 +1,16 @@
+package kr.dagger.networkcheckexample
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface NetworkModule {
+
+	@Binds
+	fun bindsNetworkMonitor(
+		networkMonitor: ConnectivityManagerNetworkMonitor
+	): NetworkMonitor
+}
